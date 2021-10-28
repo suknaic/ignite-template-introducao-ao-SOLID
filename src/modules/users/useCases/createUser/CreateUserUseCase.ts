29a417bf07e37419e80bec0fa866e2f1e480b649
fsuknaic <fsuknaic@hotmail.com>
2021-10-28 16:16:09 -0500

    DESAFIO CONCLUIDO 🚀

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailAlredyExists = this.usersRepository.findByEmail(email);
+
+    if (emailAlredyExists) {
+      throw new Error("Email alredy Exists");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
